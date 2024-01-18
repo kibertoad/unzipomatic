3052700b3a89dd295ab138dd07e54348848359d7
Igor Savin <iselwin@gmail.com>
2024-01-19 00:24:08 +0200

    Initial structure

---
+++ .github/pull_request_template.md
@@ -0,0 +1,9 @@
+## Changes
+
+Please describe
+
+## Checklist
+
+- [ ] Apply one of following labels; `major`, `minor`, `patch` or `skip-release`
+- [ ] I've updated the documentation, or no changes were necessary
+- [ ] I've updated the tests, or no changes were necessary
---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    watch: false,
+    environment: 'node',
+    reporters: ['verbose'],
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: [
+        'src/**/*.spec.ts',
+      ],
+      reporter: ['text'],
+      all: true,
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
+    },
+  },
+})
