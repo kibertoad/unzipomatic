6a4fba120f5499f0c9dd4394ea37c0cece1ae695
Vinícius Lourenço <contact@viniciusl.com.br>
2024-02-21 22:05:06 -0300

    test(unzipomatic): added more tests

--- lib/core/unzipomatic.spec.ts
+++ lib/core/unzipomatic.spec.ts
@@ -1,11 +1,54 @@
-import { resolve } from 'node:path'
+import { join, resolve } from 'node:path'
 
 import { FileTestHelper } from 'cli-testlab'
-import { afterEach, beforeEach, describe, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 
 import { getTestFileAsBuffer } from '../../test/utils/TestFileSource'
 
+import { readFileSync } from 'fs'
+import { existsSync, statSync } from 'node:fs'
+import { type TestCase, testCases } from '../../test/test-cases'
 import { unzipToFilesystem } from './unzipomatic'
+
+function ensureExpectedFilesExist(testCase: TestCase, targetDir: string): void {
+  if (testCase.expect.success !== true) {
+    throw new Error('ensureExpectedFilesExist should only be called for successful test cases')
+  }
+
+  if (testCase.expect.success && testCase.expect.files.length === 0) {
+    expect(
+      existsSync(targetDir),
+      `Files found in ${targetDir} for testCase: ${testCase.name}`,
+    ).toBe(false)
+  }
+
+  for (const file of testCase.expect.files) {
+    const path = join(targetDir, file.name)
+
+    expect(existsSync(path), `${path} do not exists for testCase: ${testCase.name}`).toBe(true)
+
+    const stats = statSync(path)
+
+    switch (file.type) {
+      case 'file': {
+        expect(stats.isFile(), `${path} is not a file for testCase: ${testCase.name}`).toBe(true)
+
+        const readContent = readFileSync(path, 'utf8')
+        expect(readContent, `${path} has unexpected content for testCase: ${testCase.name}`).toBe(
+          file.content,
+        )
+        break
+      }
+
+      case 'directory': {
+        expect(
+          stats.isDirectory(),
+          `${path} is not a directory for testCase: ${testCase.name}`,
+        ).toBe(true)
+      }
+    }
+  }
+}
 
 describe('unzipomatic', () => {
   let fileHelper: FileTestHelper
@@ -20,20 +63,31 @@ describe('unzipomatic', () => {
   describe('unzipToFilesystem', () => {
     const targetDir = resolve(__dirname, 'target')
 
-    it('Unzips file from buffer', async () => {
-      const input = await getTestFileAsBuffer('deflate.zip')
+    for (const testCase of testCases) {
+      const itFn = testCase.expect.success === 'flaky' ? it.skip : it
+      const category =
+        testCase.expect.success === true
+          ? 'ok'
+          : testCase.expect.success === 'flaky'
+            ? 'flaky'
+            : 'err'
 
-      await unzipToFilesystem(input, targetDir, {})
+      itFn(`[${category}] Unzip ${testCase.name}`, async () => {
+        const input = await getTestFileAsBuffer(testCase.path)
 
-      fileHelper.registerForCleanup(targetDir)
-    })
+        const result = await unzipToFilesystem(input, targetDir, testCase.options || {}).catch(
+          (err: Error) => err,
+        )
 
-    it('Unzips directories from buffer', async () => {
-      const input = await getTestFileAsBuffer('directories.zip')
+        if (testCase.expect.success) {
+          ensureExpectedFilesExist(testCase, targetDir)
+        } else {
+          expect(result).toBeInstanceOf(Error)
+          expect(result!.message).toMatchObject(testCase.expect.error!)
+        }
 
-      await unzipToFilesystem(input, targetDir, {})
-
-      fileHelper.registerForCleanup(targetDir)
-    })
+        fileHelper.registerForCleanup(targetDir)
+      })
+    }
   })
 })
